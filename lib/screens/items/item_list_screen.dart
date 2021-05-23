b2281749d163c1ea19116ce0f243d51d89222cf2
Miguel Caraballo de la Rosa <mcaraballodelarosa@outlook.com>
2021-05-23 21:34:14 +0200

    Items list #2
    - Moved item service from data and domain to presentation because Firebase is considered a framework.

---
+++ lib/screens/items/item_list_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class ItemListScreen extends StatelessWidget {
+  const ItemListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
--- packages/domain/lib/operations/item/item_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:domain/operations/item/item_business.dart';
-import 'package:domain/operations/item/item_failure.dart';
-
-abstract class ItemRepository {
-  Future<Either<ItemFailure, List<ItemBusiness>>> getItems();
-}
